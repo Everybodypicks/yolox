a5a068a0111fba16dbc022f7a28abf1aa96a1c42
everybodypicks <everybodypicks@outlook.com>
2022-04-23 16:43:08 +0800

    new

---
+++ yolox/data/datasets/my_dataset.py
@@ -0,0 +1,13 @@
+from pycocotools.coco import COCO
+
+
+
+class MyDataSet(COCO):
+    def __init__(self, annotation_file=None):
+        """
+        Constructor of Microsoft COCO helper class for reading and visualizing annotations.
+        :param annotation_file (str): location of annotation file
+        :param image_folder (str): location to the folder that hosts images.
+        :return:
+        """
+        # load dataset
